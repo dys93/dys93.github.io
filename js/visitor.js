b09e8d83b90e60eeb8e65020cc1b4618ca508636
dys93 <yushengding93@gmail.com>
2016-12-09 18:32:09 +0800

    server

--- js/visitor.js
+++ js/visitor.js
@@ -1,20 +1,19 @@
-$(window).load (
-    $.getJSON('http://freegeoip.net/json/?callback=?', function(data) {
-        data['url'] = document.URL;
-        $.ajax({
-            type: 'POST',
-            dataType: "jsonp",
-            url: "http://server.ysding.xyz/writeip",
-            crossDomain: true,
-            data: data,
-            dataType: 'json',
-            error: function (responseData, textStatus, errorThrown) {
-                console.log('POST failed.');
-            },
-            success: function(responseData, textStatus){
-              console.log('Success')
-            }
+$.getJSON('http://freegeoip.net/json/?callback=?', function(data) {
+    data['url'] = document.URL;
+    $.ajax({
+        type: 'POST',
+        dataType: "jsonp",
+        url: "http://server.ysding.xyz/writeip",
+        crossDomain: true,
+        data: data,
+        dataType: 'json',
+        error: function (responseData, textStatus, errorThrown) {
+            console.log('POST failed.');
+        },
+        success: function(responseData, textStatus){
+          console.log('Success')
+        }
 
-        });
-    });   
-)
+    });
+});   
+
